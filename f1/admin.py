a0ce56fc6aefb7c6167bef29b35430dc1407b1cb
Oriol-Farras <158330943+Oriol-Farras@users.noreply.github.com>
2025-02-25 18:44:38 +0100

    fix(admin): remove duplicate model registration

--- f1/admin.py
+++ f1/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Team, Driver, Circuit
 
-admin.site.register(Team)
-admin.site.register(Driver)
-admin.site.register(Circuit)
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
