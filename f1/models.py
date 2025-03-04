3b03ef6a9afa60831a986d3f8db4b5470494f64b
Oriol-Farras <158330943+Oriol-Farras@users.noreply.github.com>
2025-03-04 17:58:23 +0100

    chore(lint): fix long line and ensure __str__ methods return strings

--- f1/models.py
+++ f1/models.py
@@ -1,7 +1,11 @@
+"""Database models for the F1 Django application."""
+
 from django.db import models
 
 
 class Team(models.Model):
+    """Represents an F1 team with relevant attributes."""
+
     name = models.CharField(max_length=100, unique=True)
     headquarters = models.CharField(max_length=100)
     engine = models.CharField(max_length=100)
@@ -9,10 +13,13 @@ class Team(models.Model):
     logo = models.ImageField(upload_to="team_logos/", null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        """Returns a string representation of the team."""
+        return str(self.name)
 
 
 class Driver(models.Model):
+    """Represents an F1 driver with relevant attributes."""
+
     name = models.CharField(max_length=100)
     number = models.IntegerField(unique=True)
     nationality = models.CharField(max_length=50)
@@ -25,16 +32,26 @@ class Driver(models.Model):
     photo = models.ImageField(upload_to="driver_photos/", null=True, blank=True)
 
     def __str__(self):
+        """Returns a string representation of the driver."""
         return f"{self.name} ({self.number})"
 
 
 class Circuit(models.Model):
+    """Represents an F1 circuit with relevant attributes."""
+
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     date = models.DateField()
-    winner = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True, blank=True, related_name="circuit_wins")
+    winner = models.ForeignKey(
+        Driver,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="circuit_wins"
+    )
     image = models.ImageField(upload_to="circuit_images/", null=True, blank=True)
 
     def __str__(self):
+        """Returns a string representation of the circuit."""
         return f"{self.name} - {self.date}"
