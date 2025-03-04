bd8de644dc01ccaea54aab669196bf7947bf9d21
Oriol-Farras <158330943+Oriol-Farras@users.noreply.github.com>
2025-03-04 17:52:25 +0100

    chore(lint): add missing docstrings in apps.py module and class

--- f1/apps.py
+++ f1/apps.py
@@ -1,6 +1,9 @@
+"""App configuration for the F1 Django application."""
+
 from django.apps import AppConfig
 
 
 class F1Config(AppConfig):
+    """Configuration class for the F1 application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'f1'
